ccba7f5d155030b8ae0e1ae1edaa7c8901cab421
OKQ550 <okq550@gmail.com>
2017-12-25 02:26:53 +0200

    First Commit.

--- app/src/main/java/com/example/osamahqawasmeh/eatit/MainActivity.java
+++ app/src/main/java/com/example/osamahqawasmeh/eatit/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.osamahqawasmeh.eatit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnSignUp, btnSingIn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        assignControls();
+
     }
+
+    private void assignControls(){
+        this.btnSignUp = (Button)findViewById(R.layout.btnSignUp);
+        this.btnSingIn = (Button)findViewById(R.layout.btnSingIn);
+
+    }
+
 }
